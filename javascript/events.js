14e163553ef863c8e2cb38a30b80a70cb7b3f81a
kassycodes13 <ichokusomtoo12@gmail.com>
2024-08-23 14:15:31 +0100

    Event listeners

--- javascript/events.js
+++ javascript/events.js
@@ -1,3 +1,10 @@
+const btn = document.querySelector('.btnDark');
+const body = document.querySelector('body')
+btn.addEventListener('click', () => {
+    // body.style.backgroundColor = 'black';
+    // body.style.color = 'white';
+    body.classList.toggle('newp-class')
+})
 let h1WithId = document.getElementById("h1el")
 console.log(h1WithId);
 
@@ -20,6 +27,61 @@ sectionMain.classList = 'hello2'
 console.log(h4);
 console.log(sectionMain);
 
+// DOM MANIPULATION CONTD
+const todoItems = [
+    "Go to class",
+    "Watch some tutorials",
+    "Do some coding challenges",
+    "repeat step one",
+    "another todo"
+];
+
+todoItems.forEach((text) => {
+    let ptag = document.createElement('p');
+    ptag.textContent = text;
+    console.log(text);
+    sectionMain.append(ptag);
+})
+
 document.body.append(sectionMain);
-const dom = document.querySelector('.dom')
-dom.insertAdjacentElement("beforeend", sectionMain)
+const dom = document.querySelector('.dom');
+dom.insertAdjacentElement("beforeend", sectionMain);
+
+// REMOVING ELEMTS FROM THE DOM
+sectionMain.removeChild(sectionMain.lastElementChild)
+// sectionMain.removeChild(sectionMain.children[1].replaceChildren('hell0'))
+
+// MODIFYING ELEMENTS CLASSES
+let pClass = document.querySelector('.newp-class');
+// pClass.className += ' heloo'
+// pClass.classList.toggle('newp-class')
+pClass.style.fontSize = '30px'
+console.log(pClass.style);
+
+// EVENTS
+// Syntax for event listener 
+// object.addeventListener(event, function () {
+//     //code to be executed when the event is triggered
+// })
+const doSomething = () => {
+    console.log('hello world')
+}
+doSomething()
+const pInDivClass = document.querySelector('.events p ')
+// pInDivClass.addEventListener('click', doSomething)
+
+// pInDivClass.addEventListener('click', function () {
+//     console.log('hello world')
+// })
+const anotherDoSomething = document.querySelector('.events h2 ')
+anotherDoSomething.addEventListener('click', doSomething);
+
+pInDivClass.addEventListener('click', function (event) {
+    console.log(event)
+    console.log('hello world')
+})
+const inputEl = document.querySelector('input');
+inputEl.addEventListener('blur', (ev) => {
+
+    console.log(ev.target.value)
+})
